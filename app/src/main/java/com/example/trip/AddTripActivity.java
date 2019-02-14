99488dd9c4870fb14aa37dd7b9e1a4611a868e91
tokaaliamien <tokaaliamien@example.com>
2019-02-14 09:44:09 +0200

    Add trip Initial Commit

--- app/src/main/java/com/example/trip/AddTripActivity.java
+++ app/src/main/java/com/example/trip/AddTripActivity.java
@@ -3,11 +3,11 @@ package com.example.trip;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class TokaActivity extends AppCompatActivity {
+public class AddTripActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_toka);
+        setContentView(R.layout.activity_add_trip);
     }
 }
